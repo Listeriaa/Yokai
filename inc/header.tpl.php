e5f8d88b5a106b7b40d6a943d57e7c26ca6c5942
Listeriaa <laeti.rd@gmail.com>
2021-03-17 15:58:48 +0100

    menu déroulant CSS, transition a terminer

---
+++ inc/header.tpl.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?=$title?></title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <header>
+        <h1>Yokai on the watch</h1>
+        <?php
+        include 'inc/nav.tpl.php';
+        ?>
+    </header>
--- inc/nav.tpl.php
+++ inc/nav.tpl.php
@@ -2,46 +2,46 @@
 include 'data.php';
 $nav = [
     "Accueil"   => "index.php",
-    "Yokai"=> [],
+    "Yokai" => [],
     "Aller plus loin"   => "aller_plus_loin.php"
 ];
 
 // $tableau=$nav["Yokai"];
-foreach($yokais as $yokai => $details) {
-    
-    $tableau[$details['nom']]="article.php?article=$yokai";
+foreach ($yokais as $yokai => $details) {
 
-    
+    $tableau[$details['nom']] = "article.php?article=$yokai";
 };
 
-$nav['Yokai']=$tableau;
+$nav['Yokai'] = $tableau;
 
 ?>
 
 <nav>
     <ul>
-    <?php
-        foreach($nav as $page => $url):
+        <?php
+        foreach ($nav as $page => $url) :
             if (is_array($url)) :
-                ?>
-                <li><?=$page?>
-                <?php
-                foreach ($url as $nom => $url_article) :
-                    ?>
-            
-                <ul>
-                    <li><a href="<?=$url_article?>"><?=$nom?></a></li>
-                </ul>
-            </li>
-    <?php
-                endforeach;
-            
-            else:
-        
         ?>
-        <li><a href="<?=$url?>"><?=$page?></a></li>
-<?php
-        endif;
+                <li class="sousmenu"><a href="#" ><?= $page ?></a>
+                    <ul >
+                        <?php
+                        foreach ($url as $nom => $url_article) :
+                        ?>
+
+
+                            <li><a href="<?= $url_article ?>"><?= $nom ?></a></li>
+
+                        <?php
+                        endforeach;
+                        ?>
+                    </ul>
+                </li>
+            <?php
+            else :
+
+            ?>
+                <li><a href="<?= $url ?>"><?= $page ?></a></li>
+        <?php
+            endif;
         endforeach;
-    ?>
-       
+        ?>
