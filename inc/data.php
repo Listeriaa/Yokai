e2425b8eb27452992dbbba2f5d57c3fc4948ef84
Listeriaa <laeti.rd@gmail.com>
2021-03-21 16:14:35 +0100

    data ok pour 1-2-3-4 et condition dans yokai.tpl

--- inc/data.php
+++ inc/data.php
@@ -10,7 +10,7 @@ $yokais = [
 
         'habitat' => "Là où sont les humains.",
 
-        'apparence' => "Il ressemble en tout point à un chat, mais avec une longue queue. Cette queue lui permettrait de marcher sur deux pattes. A mesure que son âge et ses pouvoirs augmentent, il peut atteindre la taille d'un humain.<br>Un Bakeneko devenu très vieux et très puissant se transformera en Nekomata (猫又), encore plus méchant et dangereux. Ces derniers se reconnaissent à leur queue fourchue.",
+        'apparence' => "Il ressemble en tout point à un chat, mais avec une longue queue. Cette queue lui permettrait de marcher sur deux pattes. A mesure que son âge et ses pouvoirs augmentent, il peut atteindre la taille d'un humain.<br>Un Bakeneko devenu très vieux et très puissant se transformera en <i>Nekomata</i> (猫又), encore plus méchant et dangereux. Ces derniers se reconnaissent à leur queue fourchue.",
 
         'traduction' => "Chat Monstre",
 
@@ -25,10 +25,10 @@ $yokais = [
         'image' => "inugami.jpg",
         'japonais' => "犬神",
         'habitat' => "dans les villes et villages.",
-        'apparence' => "Ils prennent l'apparence d'un chien tout ce qu'il y a de plus ordinaire mais leur forme réelle est une tête momifiée de chien, cachée des regards par la famille de son maître (inugami-mochi).",
+        'apparence' => "Ils prennent l'apparence d'un chien tout ce qu'il y a de plus ordinaire mais leur forme réelle est une tête momifiée de chien, cachée des regards par la famille de son maître (<i>inugami-mochi</i>).",
         'traduction' => "Dieu Chien",
 
-        'origine' => "L'Inugami est un yōkai volontairement créé par un humain à qui il restera loyal pour l'eternité malgré la cruauté du processus. <br>En effet, il faudra enterrer un chien affamé, laissant juste sa tête hors du sol et en plaçant tout autour de lui de la nourriture qu'il ne peut atteindre.<br>Une fois mort, la nourriture est devenue offrande, assurant à son maître la fidélité et l'obéissance du yōkai. La tête de l'animal est alors coupée et conservée au sein du foyer.<br>Certaines légendes affirment que cette pratique était l'apanage de familles entières (inugami-mochi) et que chaque nouveau membre se voyait doté de son propre Inugami. Les yōkai étaient dès lors considérés comme des membres de la famille et obéissaient à toutes les volontés de leur maître.",
+        'origine' => "L'Inugami est un yōkai volontairement créé par un humain à qui il restera loyal pour l'eternité malgré la cruauté du processus. <br>En effet, il faudra enterrer un chien affamé, laissant juste sa tête hors du sol et en plaçant tout autour de lui de la nourriture qu'il ne peut atteindre.<br>Une fois mort, la nourriture est devenue offrande, assurant à son maître la fidélité et l'obéissance du yōkai. La tête de l'animal est alors coupée et conservée au sein du foyer.<br>Certaines légendes affirment que cette pratique était l'apanage de familles entières (<i>inugami-mochi</i>) et que chaque nouveau membre se voyait doté de son propre Inugami. Les yōkai étaient dès lors considérés comme des membres de la famille et obéissaient à toutes les volontés de leur maître.",
 
         'comportement' => "Les Inugami sont réputés pour apporter fortune et chance dans leur foyer. Ils sont également capables de prendre possession d'un humain, sur ordre de son maître. La malheureuse victime verra alors sa santé décliner, des marques de morsure apparaître, allant même jusqu'à aboyer.<br>Mais être le maître d'un Inugami se mérite, et en cas de maltraitance, il pourra se retourner contre son foyer : dans le meilleur des cas, il apportera la ruine à sa famille, et dans le pire, il dévorera son maître. "
 
@@ -36,27 +36,23 @@ $yokais = [
     3 => [
         'nom' => "Kamikiri",
         'image' => "kamikiri.jpg",
-        'japonais' => "袋狢",
-        'habitat' => "maison",
-        'apparence' => "gros et moche",
-        'traduction' => "Chat Monstre",
+        'japonais' => "髪切り",
+        'habitat' => "zones urbaines, toilettes, chambres.",
+        'apparence' => "Le Kamikiri est reconnaissable à ses mains et son bec en forme de ciseaux.<br> Il est décrit comme une petite créature humanoïde, mais on le retrouve parfois représenté sous la forme d'une mante religieuse avec le bec et les mains caractéristiques( sans doute dû à un amalgame : en effet, le nom en japonais de la mante est  « <i>kamakiri</i> »).",
+        'traduction' => "Coupeur de cheveux.",
 
-        'origine' => "Pour devenir un bakeneko, il suffit qu'un chat ordinaire réponde à trois critères : avoir au moins 13 ans, peser plus de 3.5kg, et posséder une queue anormalement longue.<br> Née sous l'époque Edo (1603-1868), cette légende a eu pour conséquence une coutume encore répandue aujourd'hui au Japon, qui consiste à couper la queue des chatons pour éviter leur transformation en chat-monstre. ",
-
-        'comportement' => "Les Bakeneko possèdent de multiples pouvoirs relativement problématiques. Ils sont capables de prendre l'apparence d'un humain  et de parler. Certaines légendes affirment que les Bakeneko peuvent dévorer leur maître, prenant son apparence ainsi que sa place dans le foyer.<br> Il est également soupçonné de pouvoir ressusciter les morts et de les manipuler pour leurs sombres desseins. Au Japon, on ne laisse pas un chat s'approcher d'un mort! <br> Ils sont également une menace pour la maisonnée qui les accueille, puisqu'ils peuvent faire apparaître des boules de feu, risquant ainsi d'incendier les habitations. "
+        'comportement' => "Un Kamikiri se nourrit exclusivement de cheveux. Plus espiègle que dangereux, il se dissimule dans les salles de bains, ou les chambres, attendant que sa victime s'endorme. Il sort alors de sa cachette, coupant les cheveux afin des les manger et repart sans que personne ne le remarque.<br>Certaines légendes racontent également qu'il pouvait profiter de la faible luminosité des ruelles d'antan la nuit et se faufiler derrière les marcheurs et leur dérober leur chevelure sans qu'ils ne s'en aperçoivent. Au moyen-âge et à l'époque Edo, avoir de longs cheveux était un signe de richesse et de beauté, alors subir une attaque du Kamikiri était très mal vécu par sa victime.<br>Il a également été conté qu'une attaque de Kamikiri pouvait être un avertissement, ce dernier attaquant alors les jeunes gens sur le point de se marier sans le savoir avec un yōkai. Les <i>Kitsune</i>, notamment, étaient réputés pour prendre l'apparence d'un homme et tromper les filles à marier. Ce genre de mariage finissant généralement dans le malheur, les Kamikiri intervenaient pour empêcher le mariage."
 
     ],
     4 => [
         'nom' => "Kappa",
         'image' => "kappa.jpg",
-        'japonais' => "袋狢",
-        'habitat' => "maison",
-        'apparence' => "gros et moche",
-        'traduction' => "Chat Monstre",
+        'japonais' => "河童",
+        'habitat' => "Rivières, lacs, puits, étangs.",
+        'apparence' => "Les Kappa ont l'apparence d'une tortue anthropomorphe, parfois avec une bouche ressemblant à un bec, de la taille d'un enfant. Adaptés à la vie aquatique, leur peau est recouverte d'écailles et leurs extremités sont palmées. Une autre particularité bizarre est d'avoir 3 anus.<br> Mais leur principale caractéristique est une espèce d'affaissement au sommet du crâne, appelé <i>sara</i>, rempli d'eau et qui leur confère leur puissance. Une de leur nourriture préférée est le concombre, donnant son nom à un type de maki farçi avec ce légume : « <i>kappamaki</i> ».",
+        'traduction' => "Garçon de la rivière.",
 
-        'origine' => "Pour devenir un bakeneko, il suffit qu'un chat ordinaire réponde à trois critères : avoir au moins 13 ans, peser plus de 3.5kg, et posséder une queue anormalement longue.<br> Née sous l'époque Edo (1603-1868), cette légende a eu pour conséquence une coutume encore répandue aujourd'hui au Japon, qui consiste à couper la queue des chatons pour éviter leur transformation en chat-monstre. ",
-
-        'comportement' => "Les Bakeneko possèdent de multiples pouvoirs relativement problématiques. Ils sont capables de prendre l'apparence d'un humain  et de parler. Certaines légendes affirment que les Bakeneko peuvent dévorer leur maître, prenant son apparence ainsi que sa place dans le foyer.<br> Il est également soupçonné de pouvoir ressusciter les morts et de les manipuler pour leurs sombres desseins. Au Japon, on ne laisse pas un chat s'approcher d'un mort! <br> Ils sont également une menace pour la maisonnée qui les accueille, puisqu'ils peuvent faire apparaître des boules de feu, risquant ainsi d'incendier les habitations. "
+        'comportement' => "Dans la religion Shinto, les Kappa sont souvent considérés comme un avatar de la Divinité de l'eau et des temples lui sont consacrés (dans les préfectures d'Aomori ou de Miyagi notamment). On leur attribue de grandes connaissances médicinales qu'ils auraient enseigné aux humains.<br> Mais les Kappa sont également connus pour leurs comportements malicieux, et ils n'aiment rien moins que de passer devant les passants en lâchant des gaz bruyants (l'avantage d'avoir 3 anus), voler de la nourriture ou regarder sous le kimono des femmes.<br>Mais les Kappa restent des monstres aquatiques et ils ont souvent été blamés par le passé pour avoir causé nombre de noyades ou de kidnapping d'enfants. Les adultes utilisaient (et utilisent encore) les Kappa pour avertir leurs rejetons de ne pas s'approcher des bords d'eau environnants.<br>Heureusement, les Kappa sont connus pour être des créatures polies, et si vous vous retrouvez face à un Kappa un jour, inclinez vous bien bas pour le saluer. Il n'aura pas d'autres choix que de vous rendre la politesse, renversant ainsi toute l'eau de son <i>sara</i> et le rendant inoffensif."
 
     ],
     5 => [
--- inc/yokai.tpl.php
+++ inc/yokai.tpl.php
@@ -8,9 +8,14 @@
         <p><span>Traduction</span> : <?=$yokais[$yokai]["traduction"]?></p>
 
         <p><span>Habitat</span> : <?=$yokais[$yokai]["habitat"]?></p>
-
+         <?php
+         //Je n'affiche ce paragraphe que si la clé "origine" existe dans le sous-tableau du yokai
+         if (array_key_exists('origine', $yokais[$yokai])):
+        ?>
         <p><span>Origines</span> : <?=$yokais[$yokai]["origine"]?></p>
-    
+        <?php
+        endif;
+        ?>
         <p><span>Apparence</span> : <?=$yokais[$yokai]["apparence"]?></p>
     
         <p><span>Comportement</span> : <?=$yokais[$yokai]["comportement"]?></p>
